f983789bc65295ce106670cbecfd786e31aa1fe8
Mariangélica Useche <mariangelica.useche@bureapr.com>
2020-04-03 18:39:18 -0500

    Initial commit :)

---
+++ README.md
@@ -0,0 +1,2 @@
+# Platzi video
+Proyecto del [Curso Práctico de React.js](https://platzi.com/clases/react-ejs/)
---
+++ src/index.js
@@ -0,0 +1,5 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import './assets/styles/app.scss';
+
+ReactDOM.render(<div>Hola :D</div>, document.getElementById('root'));
