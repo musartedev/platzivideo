1302627385d1a3af8ba1db716986085ff17a797a
Mariangélica Useche <mariangelica.useche@bureapr.com>
2020-04-03 19:53:08 -0500

    add Search component

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import Header from '../components/Header';
+import Search from "../components/Search";
 
 import '../assets/styles/app.scss';
 
@@ -8,6 +9,7 @@ export default class App extends Component {
         return (
             <div>
                 <Header />
+                <Search />
             </div>
         )
     }
