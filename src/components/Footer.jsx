984c6ee0afefa18195eec608b4193179e283adae
Mariangélica Useche <mariangelica.useche@bureapr.com>
2020-04-03 21:06:56 -0500

    add sass vars

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,7 +4,7 @@ import '../assets/styles/components/Footer.scss';
 
 const Footer = () => {
   return (
-    <footer class="footer">
+    <footer className="footer">
       <a href="/">Terminos de uso</a>
       <a href="/">Declaración de privacidad</a>
       <a href="/">Centro de ayuda</a>
